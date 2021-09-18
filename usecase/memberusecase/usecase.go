194985790e127fd2a3514851bfa58e6f04b09250
keitakn <keita.koga.work@gmail.com>
2021-09-18 23:43:58 +0900

    :recycle: #61 既存のテストケースを修正するのは時間がかかりそうなので新しくusecase層を定義

---
+++ usecase/memberusecase/usecase.go
@@ -0,0 +1,65 @@
+package memberusecase
+
+import (
+	"github.com/nekochans/portfolio-backend/domain"
+	Openapi "github.com/nekochans/portfolio-backend/openapi"
+)
+
+type UseCase struct {
+	MemberRepository domain.MemberRepository
+}
+
+type MemberFetchRequest struct {
+	Id int
+}
+
+func (u *UseCase) FetchFromMysql(req MemberFetchRequest) (*Openapi.Member, error) {
+	res, err := u.MemberRepository.Find(req.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+type MemberFetchAllResponse struct {
+	Items domain.Members `json:"items"`
+}
+
+func (u *UseCase) FetchAll() *MemberFetchAllResponse {
+	var item domain.Members
+
+	const keitaMemberId = 1
+	const mopMemberId = 2
+
+	item = append(
+		item,
+		&Openapi.Member{
+			Id:             keitaMemberId,
+			GithubUserName: "keitakn",
+			GithubPicture:  "https://avatars1.githubusercontent.com/u/11032365?s=460&v=4",
+			CvUrl:          "https://github.com/keitakn/cv",
+		},
+	)
+
+	item = append(
+		item,
+		&Openapi.Member{
+			Id:             mopMemberId,
+			GithubUserName: "kobayashi-m42",
+			GithubPicture:  "https://avatars0.githubusercontent.com/u/32682645?s=460&v=4",
+			CvUrl:          "https://github.com/kobayashi-m42/cv",
+		},
+	)
+
+	return &MemberFetchAllResponse{Items: item}
+}
+
+func (u *UseCase) FetchAllFromMysql() (*MemberFetchAllResponse, error) {
+	res, err := u.MemberRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return &MemberFetchAllResponse{Items: res}, nil
+}
---
+++ usecase/memberusecase/usecase_test.go
@@ -0,0 +1,64 @@
+package memberusecase
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/nekochans/portfolio-backend/infrastructure/repository"
+	Openapi "github.com/nekochans/portfolio-backend/openapi"
+	"github.com/nekochans/portfolio-backend/test"
+)
+
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	dbCreator := &test.DbCreator{}
+	db, _ = dbCreator.Create()
+
+	seeder := &test.Seeder{Db: db}
+	_ = seeder.TruncateAllTable()
+
+	status := m.Run()
+
+	os.Exit(status)
+}
+
+func TestFetchFromMysqlHandler(t *testing.T) {
+	t.Run("Success Fetch Member", func(t *testing.T) {
+		testDataDir, err := filepath.Abs("../../test/data/memberscenario/fetchfrommysql/succeed")
+		if err != nil {
+			t.Fatal("Failed Read test data", err)
+		}
+
+		seeder := &test.Seeder{Db: db, DirPath: testDataDir}
+
+		err = seeder.Execute()
+		if err != nil {
+			t.Fatal("Failed seeder.Execute()", err)
+		}
+
+		expected := &Openapi.Member{
+			Id:             1,
+			GithubUserName: "keitakn",
+			GithubPicture:  "https://avatars3.githubusercontent.com/u/11032365",
+			CvUrl:          "https://github.com/keitakn/cv",
+		}
+
+		repo := &repository.MysqlMemberRepository{Db: db}
+		u := &UseCase{MemberRepository: repo}
+		req := &MemberFetchRequest{Id: 1}
+
+		res, err := u.FetchFromMysql(*req)
+
+		if err != nil {
+			t.Error("\nActually: ", err, "\nExpected: ", expected)
+		}
+
+		if reflect.DeepEqual(res, expected) == false {
+			t.Error("\nActually: ", res, "\nExpected: ", expected)
+		}
+	})
+}
